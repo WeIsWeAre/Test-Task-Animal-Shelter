9e56d728e19d63211974f0f6641c5eca80d8e91e
WeisWeAre <robokop35@mail.ru>
2022-12-20 21:33:41 +0300

    models, serializers

--- AnimalShelter/animals/models.py
+++ AnimalShelter/animals/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+class AnimalType(models.Model):
+    name_type = models.CharField(max_length=25, blank = True, null = False)
 
-# Create your models here.
+    def __str__ (self):
+        return self.name_type
+class Animal(models.Model):
+    name = models.CharField(max_length=25, blank = True, null = False)
+    name_type = models.ForeignKey(AnimalType,related_name='animals',null = True, on_delete=models.SET_NULL)
+    weight = models.IntegerField(default = 0)
+
+    def __str__ (self):
+        return self.name
---
+++ AnimalShelter/animals/serializers.py
@@ -0,0 +1,14 @@
+from .models import AnimalType,Animal
+from rest_framework import serializers
+
+class AnimalsListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Animal
+        fields = '__all__'
+
+class AnimalTypesListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AnimalType
+        fields = '__all__'
