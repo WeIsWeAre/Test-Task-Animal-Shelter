3ef51009da35067fa38800533c44372fbedd4200
WeisWeAre <robokop35@mail.ru>
2022-12-23 14:37:05 +0300

    +

--- AnimalShelter.bat
+++ AnimalShelter.bat
@@ -3,6 +3,10 @@ set /P var="Do you have a python 3.11? Y/N "
 if %var%==N (
  python-3.11.1-amd64.exe /quiet /passive InstallAllUsers=1 PrependPath=1 Include_test=0 SimpleInstallDescription="Please, wait"
 )
+set /P var="Do you have a node.js? Y/N "
+if %var%==N (
+ start node-v18.12.1-x64.msi
+)
 start http://localhost:8080/animal_shelter
 cd myvenv/Scripts
 start activate1.bat 
